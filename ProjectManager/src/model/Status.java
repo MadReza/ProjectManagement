f1707429faa2db0d17a84c09ac9d6755b8576675
stephodev <ornella-d@hotmail.com>
2015-06-03 12:08:33 -0400

    Removing All files.

--- ProjectManager/src/model/Status.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public enum Status {
-	LOCKED,				// Prerequisites unmade
-	UNLOCKED,			// Prerequisites made, could be started any time
-	IN_PROGRESS,		// Started but has not been finished yet 
-	COMPLETED;
-	
-}
