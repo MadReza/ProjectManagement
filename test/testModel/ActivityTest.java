45e6ae297c04ab02fa2ee4d53b1346ed67cfb714
stephodev <ornella-d@hotmail.com>
2015-07-20 19:48:35 -0400

    Added test methods in ActivityTest

--- test/testModel/ActivityTest.java
+++ test/testModel/ActivityTest.java
@@ -2,7 +2,10 @@ package testModel;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import model.Activity;
+import model.Member;
 import model.Status;
 
 import org.junit.After;
@@ -35,5 +38,49 @@ public class ActivityTest {
 		assertEquals("Both activities should be equal", true, activity.equals(_activity2));
 		
 	}
+	
+	//This test method also tests setActivityTeam() in the process of testing  getActivityteam().
+	@Test
+	public void testGetActivityTeam() throws Exception {
+		Activity activity = new Activity(1, "ActivityA", "Description", 10000.0, "Mon Jul 20 12:00:00 EDT 2015", "Mon Jul 27 12:00:00 EDT 2015", Status.LOCKED);
+		Member member = new Member("a","a");
+		Member _member = new Member("b","b");
+		
+		ArrayList<Member> team = new ArrayList<Member>();
+		team.add(member);
+		team.add(_member);
+		
+		activity.setActivityTeam(team);
+		assertEquals("Activity team arraylist should be equals", team, activity.getActivityTeam());
+		
+	}
+	
+	//Tested above in testGetActivityTeam()
+	@Test
+	public void testSetActivityTeam() throws Exception {
+	}
+	
+	//Tested below in testRemoveMemberFromTeam()
+	@Test
+	public void testAddMemberToTeam() throws Exception {
+	}
+	
+	//This method adds members in order to remove and hence tests both addMemberToTeam() and 
+	// removeMemberFromTeam() methods.
+	@Test
+	public void testRemoveMemberFromTeam() throws Exception {
+		Activity activity = new Activity(1, "ActivityA", "Description", 10000.0, "Mon Jul 20 12:00:00 EDT 2015", "Mon Jul 27 12:00:00 EDT 2015", Status.LOCKED);
+		Member member = new Member("a","a");
+		Member _member = new Member("b","b");
+		
+		activity.addMemberToTeam(member);
+		activity.addMemberToTeam(_member);
+		
+		activity.removeMemberFromTeam(_member);
+		
+		assertEquals("Activity Team should be equal 1", 1, activity.getActivityTeam().size());
+	
+	}
+
 
 }
